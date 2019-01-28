f90dd91ebfc99d17049413737d921ed481397b0d
Nathan Glover <nathan@glovers.id.au>
2019-01-28 19:12:41 +0800

    Finished basic implementation

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # Udemy Video Converter - FFMPEG Lambda Layer
 
-## Deploy Lambda Layer
-
-Edit the first few lines in `ffmpeg-aws-lambda-layer/Makefile`
+Deploy the base image to an S3 bucket you have available
 
 ```bash
-cd ffmpeg-aws-lambda-layer
+aws s3 cp assets/icon.png s3://devopstar/resources/aws-ffmpeg-serverless-convert/base/icon.png
+```
+
+## Deploy Lambda Layer
+
+Edit the first few lines in `lambda-layer/Makefile`
+
+```bash
+cd lambda-layer
 make deploy
 ```
 
@@ -18,4 +24,10 @@ Deploy the stack
 ```bash
 cd video-converter
 make deploy
+```
+
+## Upload Demo Video
+
+```bash
+aws s3 cp assets/demo.mp4 s3://uploads-ffmpeg-video-converter/demo.mp4
 ```
--- video-converter/src/index.js
+++ video-converter/src/index.js
@@ -1,30 +1,35 @@
-
 const s3Util = require('./s3-util'),
-	childProcessPromise = require('./child-process-promise'),
-	path = require('path'),
-	os = require('os'),
-	EXTENSION = process.env.EXTENSION,
-	THUMB_WIDTH = process.env.THUMB_WIDTH,
-	OUTPUT_BUCKET = process.env.OUTPUT_BUCKET,
-	MIME_TYPE =  process.env.MIME_TYPE;
+    childProcessPromise = require('./child-process-promise'),
+    path = require('path'),
+    os = require('os'),
+    EXTENSION = process.env.EXTENSION,
+    OUTPUT_BUCKET = process.env.OUTPUT_BUCKET,
+    MIME_TYPE = process.env.MIME_TYPE,
+    RESOURCE_BUCKET = process.env.RESOURCE_BUCKET,
+    ICON_FILE = process.env.ICON_FILE;
 
 exports.handler = function (eventObject, context) {
-	const eventRecord = eventObject.Records && eventObject.Records[0],
-		inputBucket = eventRecord.s3.bucket.name,
-		key = eventRecord.s3.object.key,
-		id = context.awsRequestId,
-		resultKey = key.replace(/\.[^.]+$/, EXTENSION),
-		workdir = os.tmpdir(),
-		inputFile = path.join(workdir,  id + path.extname(key)),
-		outputFile = path.join(workdir, 'converted-' + id + EXTENSION);
+    const eventRecord = eventObject.Records && eventObject.Records[0],
+        inputBucket = eventRecord.s3.bucket.name,
+        key = eventRecord.s3.object.key,
+        id = context.awsRequestId,
+        resultKey = key.replace(/\.[^.]+$/, EXTENSION),
+        workdir = os.tmpdir(),
+        inputFile = path.join(workdir, id + path.extname(key)),
+        iconFile = path.join(workdir, "icon.png"),
+        watermarkerFile = path.join(workdir, 'watermarked-' + id + EXTENSION);
 
-
-	console.log('converting', inputBucket, key, 'using', inputFile);
-	return s3Util.downloadFileFromS3(inputBucket, key, inputFile)
-		.then(() => childProcessPromise.spawn(
-			'/opt/bin/ffmpeg',
-			['-loglevel', 'error', '-y', '-i', inputFile, '-vf', `thumbnail,scale=${THUMB_WIDTH}:-1`, '-frames:v', '1', outputFile],
-			{env: process.env, cwd: workdir}
-		))
-		.then(() => s3Util.uploadFileToS3(OUTPUT_BUCKET, resultKey, outputFile, MIME_TYPE));
+    s3Util.downloadFileFromS3(RESOURCE_BUCKET, ICON_FILE, iconFile)
+        .then(() => {
+            console.log('converting', inputBucket, key, 'using', inputFile);
+            return s3Util.downloadFileFromS3(inputBucket, key, inputFile)
+                .then(() => childProcessPromise.spawn(
+                    '/opt/bin/ffmpeg',
+                    ['-loglevel', 'error', '-i', inputFile, '-i', iconFile, '-filter_complex', 'overlay=10:main_h-overlay_h-10', watermarkerFile], {
+                        env: process.env,
+                        cwd: workdir
+                    }
+                ))
+                .then(() => s3Util.uploadFileToS3(OUTPUT_BUCKET, resultKey, watermarkerFile, MIME_TYPE));
+        })
 };
